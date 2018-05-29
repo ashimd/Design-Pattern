fda473af09433dd3c62218825c7218095473ece8
Ashim <ashim.sp@gmail.com>
2018-05-29 14:27:29 +0530

    Create Program.cs

---
+++ Creational-Pattern/Object-Pattern/AbstractFactory/FoodChain/Program.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace AbstractFactory.RealWorld
+{
+    class Program
+    {
+        /// <summary>
+        /// MainApp startup class for Real-World
+        /// Abstract Factory Design Pattern.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            // Create and run the African animal world
+            ContinentFactory africa = new AfricaFactory();
+            AnimalWorld world = new AnimalWorld(africa);
+            world.RunFoodChain();
+
+            // Create and run the American animal world
+            ContinentFactory america = new AmericaFactory();
+            world = new AnimalWorld(america);
+            world.RunFoodChain();
+
+            // Wait for user input
+            Console.ReadKey();
+        }
+    }
+}
